ad6f5cabb1842c6f36f5c66ae25817b9cc50b71a
Adrian Dorneanu <adrian.dorneanu@zig.nl>
2026-08-15 11:06:49 +0200

    Replaced deprecated z.string.email with z.email

--- src/features/auth/schemas/authSchema.ts
+++ src/features/auth/schemas/authSchema.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const loginSchema = z.object({
-	email: z.string().min(1, "Email is required.").email("Enter a valid email."),
+	email: z.email("Enter a valid email.").min(1, "Email is required"),
 	password: z.string().min(1, "Password is required."),
 });
 
